d2409fb8a2e56d630e8c1bf357075709fc2c9994
Redalaaouaj <redalaaouaj1@gmail.com>
2023-10-09 14:16:56 -0700

    alx

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Description: print 0, 1, - 9
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int d = 0;
+
+	while (d <= 9)
+	{
+		putchar(d + 48);
+
+		if (d != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		d++;
+	}
+	putchar('\n');
+
+	return (0);
+}
