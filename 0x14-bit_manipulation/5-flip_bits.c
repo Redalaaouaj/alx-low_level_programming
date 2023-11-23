dcb3b5ef7ab052ee8fa22498e8e5c1329ad5ae49
Redalaaouaj <redalaaouaj1@gmail.com>
2023-11-23 12:56:45 -0800

    alx

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,65 +1,24 @@
 #include "main.h"
-
 /**
- * print_bin - prints binary rep of @n
- *
- * @number: decimal value
- *
- * Return: nothing
-*/
-void print_bin(unsigned long int number)
-{
-	if (number >> 1)
-		print_bin(number >> 1);
-
-	putc((number & 1) ? '1' : '0', stdout);
-}
-
-/**
- * flip_bits - a function that returns the number of bits you
- *             would need to flip to get from one number to
- *             another
- *
- * @n: decimal number
- * @m: the other decimal number
- *
- * Return: number of bits to flip to get @m from @n
-*/
+ * flip_bits - returns the differeces about digits between 2 numbers.
+ * @n: the input number1
+ * @m: input number 2
+ * Return: the diferences
+ */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int diff_bits = 0;
-	unsigned long int diff;
+	unsigned long int bit_diff, mask = 0;
+	int i, digit;
+	unsigned int differences = 0;
 
-	/*xor n and b*/
-	diff = n ^ m;
+	bit_diff = n ^ m;/*1 if bit_n != bit_m (XOR)*/
 
-	#ifdef DEBUG
-	printf("\n(n) %ld and (m) %ld in binary is ", n, m);
-	print_bin(n);
-	printf(" ");
-	print_bin(m);
-	printf(" diff is %ld and in binary is ", diff);
-	print_bin(diff);
-	printf("\n");
-	#endif
-
-	/*keep shifting diff to right*/
-	do {
-		#ifdef DEBUG
-		printf("diff %ld before right shift ", diff);
-		print_bin(diff);
-		printf("\n");
-		#endif
-
-		diff_bits += (diff & 1);
-		diff >>= 1;
-
-		#ifdef DEBUG
-		printf("diff %ld after right shift ", diff);
-		print_bin(diff);
-		printf(" and diff_bit %d before leaving loop\n\n", diff_bits);
-		#endif
-	} while (diff > 0);
-
-	return (diff_bits);
+	for (i = 63; i >= 0; i--)
+	{
+		mask = 1 << i;/*Creating a mask for extract the digit*/
+		digit = (bit_diff & mask) >> i;/*Extract the digit and push to 0 or 1*/
+		if (digit == 1)
+			differences++;
+	}
+	return (differences);
 }
